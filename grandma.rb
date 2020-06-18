777a20c1eb18e4ce85bdd2fd69b02cc385c20988
Andriana Garrido <andriana.amg@gmail.com>
2020-06-18 06:02:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,7 @@
 def speak_to_grandma(speak)
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
-  elsif
+  else
     "HUH?! SPEAK UP, SONNY!"
-  else speak == speak
-    "I LOVE YOU TOO PUMPKIN!"
   end
 end
